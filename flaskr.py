b68dca66d8bc0f399395a0cb000ab186c7398b87
Chanathip Srithanrat <chanathip.srithanrat@thomsonreuters.com>
2016-04-29 11:28:55 +0700

    Create database

--- flaskr.py
+++ flaskr.py
@@ -1,4 +1,5 @@
 import sqlite3
+from contextlib import closing
 
 from flask import Flask, request, session, g, redirect, url_for, abort, \
         render_template, flash
@@ -12,6 +13,12 @@ app.config.from_object(config)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def init_db():
+    with closing(connect_db()) as db:
+        with app.open_resource('schema.sql', mode='r') as f:
+            db.cursor().executescript(f.read())
+            db.commit()
 
 
 if __name__ == '__main__':
